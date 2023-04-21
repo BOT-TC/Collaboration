771fa0a045ef4154e0ea2c1feb25a16a36dd6a16
John Farrar <johnfarrar@sosapps.com>
2023-04-21 19:17:14 -0400

    Missed update to E2E test

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,10 +3,11 @@
 describe('My First Test', () => {
   it('visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'You did it!')
-    /* ==== Generated with Cypress Studio ==== */
+    cy.contains('h1', 'Modern Vue Dev')
     cy.get('[href="/about"]').click();
     cy.contains('h1', 'This is an about page')
-    /* ==== End Cypress Studio ==== */
+    cy.get('[href="/login"]').click();
+    cy.get('#app > :nth-child(2) > div > h1').click();
+    cy.contains('h1', 'Sign In/Up')
   })
 })
