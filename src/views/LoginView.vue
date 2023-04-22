14e1f2710846248835442ba45cb475a119184771
John Farrar <johnfarrar@sosapps.com>
2023-04-22 15:05:49 -0400

    Part work E2E tests to follow.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -65,16 +65,56 @@ const clearFormData = () => {
 
 <template>
   <div>
-    <div v-if="user?.email">
-      <button @click="signOutOfFirebase">Sign Out</button>
-    </div>
-    <div v-else>
-      <h1>Sign In/Up</h1>
-      <form @submit.prevent>
-        <input type="email" placeholder="email" v-model="newUser.email" /><br>
-        <input type="password" v-model="newUser.password" /><br>
-        <button @click="createUser">Register</button>|<button @click="signIntoFirebase">Sign In</button>
+    <div class="flex justify-center">
+      <form @submit.prevent class="form-control max-w-lg" v-if="user?.email">
+        <button @click="signOutOfFirebase" class="btn">Sign Out</button>
       </form>
+      <div v-else>
+        <h1>Sign In/Up</h1>
+        <form @submit.prevent class="form-control max-w-lg">
+          <label for="email" class="label">
+            <span class="label-text">What is your email?</span>
+            <input id="email" type="email" placeholder="email" v-model="newUser.email"
+              class="input input-bordered w-full primary" />
+          </label><br>
+
+          <label for="password" class="label">
+            <span class="label-text">Choose a password.</span>
+            <input id="password" type="password" v-model="newUser.password" class="input input-bordered w-full" /><br>
+          </label>
+          <button @click="createUser" class="btn mt-3 bg-primary text-primary-content">Register</button>
+          <button @click="signIntoFirebase" class="btn mt-3">Sign
+            In</button>
+        </form>
+      </div>
     </div>
+
+    <div class="container shadow-lg mx-auto bg-white mt-24 md:mt-18">asdf</div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <hr>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <hr>
+    <br>
+    <br>
+    <br>
+    <br>
+    <hr>
+    <br>
+    <br>
+    <br>
+    <br>
+    <hr>
   </div>
 </template>
